130c6f6d28e702479e454ad3f436d6480aa25002
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2024-09-12 14:00:24 -0400

    CRS-12345 - Added base branch

--- git-commit-push-script.sh
+++ git-commit-push-script.sh
@@ -4,6 +4,9 @@ source ~/.bash_profile
 # Add all changes
 git add -A
 
+# Get branch name
+base_branch=$(git rev-parse --abbrev-ref HEAD)
+
 # Extract ticket number from current directory
 ticket=$(echo $base_branch | grep -o -E '([A-Za-z]+-[0-9]{3,}|[A-Za-z]+-[0-9]{3,})')
 
