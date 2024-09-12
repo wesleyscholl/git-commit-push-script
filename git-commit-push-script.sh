a1cacf036e416826a33d37c8859edf95522fca57
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2024-09-12 13:38:39 -0400

    -

---
+++ git-commit-push-script.sh
@@ -0,0 +1,20 @@
+#!/bin/bash
+
+# Create the alias
+alias cm='
+
+# Add all changes
+git add -A
+
+# Extract ticket number from current directory
+ticket=$(pwd | grep -oE "CRS-[0-9]+" | head -n 1)
+
+# Prompt for commit message
+read -p "Enter commit message: " message
+
+# Prepare and execute commit command
+git commit -S -m "$ticket - $message"
+
+# Push changes
+git push
+'
