08cd9c626b71b4687b84fbdbfc39e8348839e35f
Wesley Scholl <128409641+wesleyscholl@users.noreply.github.com>
2024-09-23 11:46:25 -0400

    TEST-1234 feat: Add Gemini-based commit message generation
    This commit introduces a new feature that generates commit messages using the Gemini API, enabling more informative and consistent commits. The script now retrieves a concise and relevant commit message from Gemini based on the staged changes, providing a more robust solution for generating commit messages.

--- git-commit-push-script.sh
+++ git-commit-push-script.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 source ~/.bash_profile
 
-# Add all changes
+# Stage all changes
 git add -A
 
 # Get branch name
@@ -9,8 +9,6 @@ base_branch=$(git rev-parse --abbrev-ref HEAD)
 
 # Extract ticket number from current directory
 ticket=$(echo $base_branch | grep -o -E '([A-Za-z]+-[0-9]{3,}|[A-Za-z]+-[0-9]{3,})')
-
-echo "Ticket: $ticket"
 
 # Get the git diff
 diff=$(git diff --cached)
@@ -22,14 +20,12 @@ diff=$(echo $diff | sed 's/\\/\\\\/g' | sed 's/"/\\"/g' | sed 's/\n/\\n/g')
 gemini_request='{"contents":[{"parts":[{"text": "Write a git commit message (72 character maximum) for the following git diff: '"$diff"' "}]}]}'
 
 # Get commit message from Gemini API
-commit_message=$(curl \
+commit_message=$(curl -s \
   -H 'Content-Type: application/json' \
   -d "$gemini_request" \
   -X POST "https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=${GEMINI_API_KEY}" \
   |  jq -r '.candidates[0].content.parts[0].text'
 )
-
-echo "$commit_message"
 
 # Prepare and execute commit command
 git commit -S -m "$ticket $commit_message"
@@ -37,12 +33,28 @@ git commit -S -m "$ticket $commit_message"
 # Check if the branch exists on the remote
 remote_branch=$(git ls-remote --heads origin $base_branch)
 
+# Function: pull_push_after_failed_push - If push fails, attempt to pull and push again
+pull_push_after_failed_push() {
+	echo "Push failed. Attempting to pull and push again."
+	git pull
+	git push
+}
+
+# Check if the branch exists on the remote
 if [ -z "$remote_branch" ]; then
 	echo "Branch '$base_branch' does not exist on remote. Creating it."
 	# Push the local branch to the remote, setting the upstream branch
 	git push --set-upstream origin $base_branch
+
+	if [ $? -ne 0 ]; then
+		pull_push_after_failed_push
+	fi
 else
 	echo "Branch '$base_branch' exists on remote. Pushing changes."
 	# Push changes to the remote
 	git push
+
+	if [ $? -ne 0 ]; then
+		pull_push_after_failed_push
+	fi
 fi
